050e93edc1d39f8b07e534828e554a58eaf6983c
Edmo Lima <edmo.lima@locaweb.com.br>
2016-09-29 20:28:39 -0300

    Remove require and add App which was previously imported.

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ const renderApp = (NextApp) => {
 renderApp(App)
 
 if (module.hot) {
-  module.hot.accept('./app', () => {
-    const NextApp = require('./app').default
+  module.hot.accept(App, () => {
+    const NextApp = App.default
     renderApp(NextApp)
   })
 }
