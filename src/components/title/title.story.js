c769d6aef42ff7a85c8454fdb1e7e29b1d343bcd
Fernando Daciuk <f.daciuk@gmail.com>
2017-02-12 00:35:39 -0200

    Add async/await story

--- src/components/title/title.story.js
+++ src/components/title/title.story.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import { storiesOf } from '@kadira/storybook'
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './index'
 
 const stories = storiesOf('Title', module)
@@ -9,3 +9,38 @@ const stories = storiesOf('Title', module)
 stories.add('without props', () => (
   <Title>Main title</Title>
 ))
+
+stories.add('async / await', () => {
+  class Main extends Component {
+    constructor () {
+      super()
+      this.state = { title: '...' }
+    }
+
+    getTitle () {
+      return new Promise((resolve, reject) => {
+        this.timer = setTimeout(() => {
+          resolve('title with async / await!')
+        }, 2000)
+      })
+    }
+
+    async componentDidMount () {
+      this.setState({
+        title: await this.getTitle()
+      })
+    }
+
+    componentWillUnmount () {
+      clearTimeout(this.timer)
+    }
+
+    render () {
+      return (
+        <Title>{this.state.title}</Title>
+      )
+    }
+  }
+
+  return <Main />
+})
