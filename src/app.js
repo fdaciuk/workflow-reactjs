050fc803590e679e1c77ca015d2ab46ffab0923e
Fernando Daciuk <f.daciuk@gmail.com>
2017-02-11 22:43:23 -0200

    Update app with async/await example

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Title from 'components/title'
 
 import './css/style.css'
 
-const App = () => (
-  <div>
-    <Title>My App</Title>
-  </div>
-)
+class App extends Component {
+  constructor () {
+    super()
+    this.state = { title: '...' }
+  }
+
+  getTitle () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('My app with async / await!')
+      }, 2000)
+    })
+  }
+
+  async componentDidMount () {
+    this.setState({
+      title: await this.getTitle()
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Title>{this.state.title}</Title>
+      </div>
+    )
+  }
+}
 
 export default App
