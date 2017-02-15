5ffee502fd4a059168adc6027f0ec8db7f2adbf3
Fernando Daciuk <f.daciuk@gmail.com>
2017-02-15 19:24:44 -0200

    Remove manual babelrc config on storybook webpack config

--- webpack/storybook.config.js
+++ webpack/storybook.config.js
@@ -12,21 +12,6 @@ module.exports = (config, env) => {
   })
 
   newConfig.module.loaders = newConfig.module.loaders.concat(preloaders)
-  newConfig.module.loaders = newConfig.module.loaders.map((loader) => {
-    if (loader.test.test('test.js') && loader.enforce !== 'pre') {
-      return Object.assign({}, loader, {
-        query: Object.assign({}, loader.query, {
-          presets: loader.query.presets.map((preset) => (
-            Array.isArray(preset) && preset[0] === 'es2015'
-              ? preset[0]
-              : preset
-          ))
-        })
-      })
-    }
-    return loader
-  })
-
   newConfig.resolve = common.resolve
 
   return newConfig
