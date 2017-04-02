8022aab4b12966eb9732c2b8d1e7f23724fdf2e2
Fernando Daciuk <f.daciuk@gmail.com>
2017-04-02 11:43:20 -0300

    Add Preact, build analyzer and improve html, minifying it

--- README.md
+++ README.md
@@ -9,7 +9,9 @@
 * Jest for tests and Chai for assertions;
 * Storybook to create isolatelly components' stories
 * Async/await
+* Dynamic `import()` function
 * Webpack tree shaking!
+* [Preact](https://preactjs.com/) for production build (optional)
 
 ## Dependencies:
 
@@ -29,7 +31,8 @@
 - `yarn test (or npm test)`: Run tests once
 - `yarn test:watch (or npm run test:watch)`: Run tests in watch mode
 - `yarn build (or npm run build)`: Build project to production
-- `yarn storybook (or npm run storybook)`: Run Storybook on 6006 port
+- `yarn build:analyzer (or npm run build:analyzer)`: Build project to production and open bundle analyzer on `8888` port
+- `yarn storybook (or npm run storybook)`: Run Storybook on `6006` port
 - `yarn build-storybook`: Build Storybook to static files
 - `yarn update-packages`: Update all packages to the latest version
 
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -6,6 +6,7 @@ const common = require('./common')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanPlugin = require('clean-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
   entry: common.entry,
@@ -29,17 +30,38 @@ module.exports = {
 
     new webpack.optimize.CommonsChunkPlugin({
       name: 'react-build',
+      chunks: ['main'],
       minChunks: ({ resource }) => (
-        /node_modules\/react(-dom)?\//.test(resource)
+        /node_modules\/(react(-dom)?|fbjs)\//.test(resource) ||
+        /node_modules\/preact(-compat)?\//.test(resource)
       )
     }),
 
-    new HtmlPlugin(common.htmlPluginConfig),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      chunks: ['main'],
+      minChunks: ({ resource }) => (
+        /node_modules/.test(resource)
+      )
+    }),
+
+    new HtmlPlugin(Object.assign({}, common.htmlPluginConfig, {
+      minify: { collapseWhitespace: true },
+
+      chunksSortMode: (chunk1, chunk2) => {
+        const order = ['react-build', 'vendor', 'main']
+        const left = order.indexOf(chunk1.names[0])
+        const right = order.indexOf(chunk2.names[0])
+        return left - right
+      }
+    })),
 
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true
     })
-  ],
+  ].concat(
+    process.env.ANALYZER ? new BundleAnalyzerPlugin() : []
+  ),
 
   module: {
     rules: [
@@ -56,5 +78,12 @@ module.exports = {
     ]
   },
 
-  resolve: common.resolve
+  // resolve: common.resolve
+
+  resolve: {
+    alias: Object.assign({}, common.resolve.alias, {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    })
+  }
 }
