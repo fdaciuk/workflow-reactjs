bbf1c662f49516df21a4a0e9121bcc858bf0a839
Fernando Daciuk <f.daciuk@gmail.com>
2017-04-18 16:34:38 -0300

    Add option to use spawn on windows

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@
 const gulp = require('gulp')
 const { spawn } = require('child_process')
 
+// on windows, install and use `cross-spawn`:
+// const { spawn } = require('cross-spawn')
+
 gulp.task('lint', (cb) => {
   const cmd = spawn('yarn', ['lint'], { stdio: 'inherit' })
   cmd.on('close', () => cb())
