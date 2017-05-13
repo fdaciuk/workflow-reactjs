53bd4516e491bb96c3a19a456c10b93818d12bbb
Fernando Daciuk <f.daciuk@gmail.com>
2017-05-13 18:54:35 -0300

    Use PureComponent instead Component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 'use strict'
 
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
 import './css/style.css'
 
-class App extends Component {
+class App extends PureComponent {
   constructor () {
     super()
     this.state = {
